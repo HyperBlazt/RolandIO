f71a7bb6472a54e5a783106fa85a89b99f35e815
Mark <markroland@youmail.com>
2016-07-10 08:15:32 +0200

    Less tilføjet

---
+++ www.RolandIO.dk/Controllers/Contact Box.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace www.RolandIO.dk.Controllers
+{
+    /// <summary>
+    /// The contact_ box.
+    /// </summary>
+    public class ContactBox
+    {
+
+    }
+}
